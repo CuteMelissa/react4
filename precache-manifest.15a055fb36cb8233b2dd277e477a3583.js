789ddc152117c063045af67dc6394d9c843d37c9
刘紫璐 <44942900+CuteMelissa@users.noreply.github.com>
2020-10-25 18:47:45 +0800

    Add files via upload

---
+++ precache-manifest.15a055fb36cb8233b2dd277e477a3583.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf2db46262f8eca1ddc6c48ebfe6f06",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57091bf869bb5629eece",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "f58cea2092a5666b09c1",
+    "url": "./static/css/main.49b74bc5.chunk.css"
+  },
+  {
+    "revision": "57091bf869bb5629eece",
+    "url": "./static/js/2.b3e07a7f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.b3e07a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58cea2092a5666b09c1",
+    "url": "./static/js/main.2b9c9363.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "806b37313e49d47ba412aa650b82bd75",
+    "url": "./static/media/danshentu1.806b3731.JPG"
+  }
+]);
